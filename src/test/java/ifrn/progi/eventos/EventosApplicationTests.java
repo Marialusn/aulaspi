57aef441774094b0949717fb73e79d96ccaaeef1
Maria Luiza <marialusn437@gmail.com>
2021-09-24 20:49:32 -0300

    Atualizações toker

---
+++ src/test/java/ifrn/progi/eventos/EventosApplicationTests.java
@@ -0,0 +1,13 @@
+package ifrn.progi.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
